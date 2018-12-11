fbdd7b3e596917af122ad821c425ef466b56c413
Unknown <dozorovmn@gmail.com>
2018-12-11 22:54:13 +0300

    123

--- my-appcreate-react-app/src/App/App.js
+++ my-appcreate-react-app/src/App/App.js
@@ -1,8 +1,26 @@
 import React from 'react';
 import Node from "../Node/Node.js";
+import utils from "../utils/utils.js";
 
 class App extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      load: false,
+    }
+  }
+
+  componentWillMount = () => {
+    utils.readTextFile("./characterInfo.json", this.loadRes);
+  }
+
+  loadRes = (response) => {
+    debugger;
+    console.dir(response);
+  }
+
   render() {
+    if (!this.state.load) return ("");
     return (
       <Node/>
   )}
--- my-appcreate-react-app/src/Node/Node.js
+++ my-appcreate-react-app/src/Node/Node.js
@@ -2,10 +2,38 @@ import React from 'react';
 import style from "./Node.css"; 
 
 class Node extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: this.props.name,
+      show: false,
+    }
+  }
+
+  renderCharacter = () => {
+    return (
+      <div>
+        Character
+      </div>
+    );
+  }
+
+  changeShow = () => {
+    this.setState({
+      show: !this.state.show,
+    })
+  }
+
   render() {
     return (
-      <div className="node">
-        hello
+      <div>
+        <div className="node" onClick={this.changeShow}>
+          hello
+        </div>
+        {
+          this.state.show &&
+            this.renderCharacter()
+        }
       </div>
     );
   }
